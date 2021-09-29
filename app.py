9d28521cf0e83676ed36c6383c07be0a1881540e
Andrei Saavedra <andreisr0105@gmail.com>
2021-09-29 12:02:21 -0500

    Version 2.0

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model_02.pickle', 'rb'))
+model = pickle.load(open('model_03.pickle', 'rb'))
+local_scaler = pickle.load(open('sc_03.pickle', 'rb'))
 
 @app.route('/')
 def home():
@@ -14,13 +15,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    prediction = model.predict(local_scaler.transform(final_features))
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='El promedio final del alumno es {}'.format(output))
+    return render_template('index.html', prediction_text='El promedio final del alumno es {}'.format(prediction))
 
 
 if __name__ == "__main__":
